7ae6fe98a54909ab2d8b2f20ca3ff34b4aef8a1c
davidw826 <53101580+davidw826@users.noreply.github.com>
2023-04-29 20:21:28 -0400

    Updated from Brython Server: 4/29/2023 8:21:27 PM

--- tic-tac-toe-classes.py
+++ tic-tac-toe-classes.py
@@ -1,1 +1,14 @@
-
+class Tile:
+    
+    def __init__(self,x,y,player):
+        self.x = x
+        self.y = y
+        self.player = player
+    
+class Board:
+    
+    def __init__(self,size,tiles):
+        tiles = []
+        for x in range(size):
+            tiles.append([Tile(x,y,' ') for y in range(size)])
+        
